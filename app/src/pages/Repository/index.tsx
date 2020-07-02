e5385b1dc0b2a4a98e1900ac6e373066a33b6f79
GermainPereira <pereira.germain@outlook.com>
2020-07-02 15:30:31 -0300

    Added new page

--- app/src/pages/Repository/index.tsx
+++ app/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft } from "react-icons/fi";
+import { Header, RepositoryInfo } from "./styles";
+
+import logo from "../../assets/logo.svg";
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,44 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/52306002?s=400&u=355e80892c7829f7e34dd3574947266607d84a5c&v=4"
+            alt="Germain Avatar"
+          />
+        </header>
+        <div>
+          <strong>{params.repository}</strong>
+          <p>Descrição do Repositório</p>
+        </div>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
---
+++ app/src/pages/Repository/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+  }
+
+  &:hover {
+    color: #111;
+  }
+  svg {
+    margin-right: 4px;
+  }
+`;
+
+export const RepositoryInfo = styled.section``;
